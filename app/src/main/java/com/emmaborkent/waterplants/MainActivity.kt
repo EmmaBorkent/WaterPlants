b69ab48eca3b469969cbfdf893544a7c92db073b
Emma Borkent <emma_borkent@msn.com>
2020-02-29 16:30:52 +0100

    Changed activity_main.xml and added BottomSheetBehavior

--- app/src/main/java/com/emmaborkent/waterplants/MainActivity.kt
+++ app/src/main/java/com/emmaborkent/waterplants/MainActivity.kt
@@ -2,11 +2,52 @@ package com.emmaborkent.waterplants
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.View
+import android.view.animation.AnimationUtils
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomSheetBehavior = BottomSheetBehavior.from<ConstraintLayout>(main_constraint_layout_bottom)
+        bottomSheet()
+
+    }
+
+    private fun bottomSheet() {
+
+        // Get display height to calculate peekHeight for the Bottom Sheet
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        // This might go wrong with screens with different pixel densities
+        val halfScreenHeight = displayMetrics.heightPixels*0.41
+        bottomSheetBehavior.peekHeight = halfScreenHeight.toInt()
+
+        bottomSheetBehavior.addBottomSheetCallback(object: BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+
+                // For state logging
+                when (newState) {
+                    BottomSheetBehavior.STATE_EXPANDED -> Log.i("STATE", "Expanded State")
+                    BottomSheetBehavior.STATE_COLLAPSED -> Log.i("STATE", "Collapsed State")
+                    BottomSheetBehavior.STATE_DRAGGING -> Log.i("STATE", "Dragging...")
+                    BottomSheetBehavior.STATE_SETTLING -> Log.i("STATE", "Settling...")
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> Log.i("STATE", "Half Expended State")
+                    BottomSheetBehavior.STATE_HIDDEN -> Log.i("STATE", "Hidden State")
+                }
+            }
+        })
+
     }
 }
