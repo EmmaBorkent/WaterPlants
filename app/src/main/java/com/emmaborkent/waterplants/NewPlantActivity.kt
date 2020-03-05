164bd0a826ec9778e90793f91415f1052322fee9
Emma Borkent <emma_borkent@msn.com>
2020-03-05 16:42:17 +0100

    Created add new plant activity

---
+++ app/src/main/java/com/emmaborkent/waterplants/NewPlantActivity.kt
@@ -0,0 +1,12 @@
+package com.emmaborkent.waterplants
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewPlantActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_plant)
+    }
+}
