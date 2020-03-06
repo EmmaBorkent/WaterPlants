13fe6e3ef173e9b319522a82adc64117df507b00
Emma Borkent <emma_borkent@msn.com>
2020-03-06 15:35:43 +0100

    Updated the layout to add a new plant and added a menu to save the new plant. Functionality not yet working.

--- app/src/main/java/com/emmaborkent/waterplants/ui/NewPlantActivity.kt
+++ app/src/main/java/com/emmaborkent/waterplants/ui/NewPlantActivity.kt
@@ -2,7 +2,11 @@ package com.emmaborkent.waterplants.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.emmaborkent.waterplants.R
+import com.emmaborkent.waterplants.database.Plant
+import com.emmaborkent.waterplants.database.PlantDatabaseHandler
 
 class NewPlantActivity : AppCompatActivity() {
 
@@ -12,5 +16,22 @@ class NewPlantActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.new_plant_toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setTitle(R.string.new_plant_toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_save -> saveNewPlant()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun saveNewPlant() {
+        val plant = Plant()
+        PlantDatabaseHandler(this).createPlant(plant)
     }
 }
