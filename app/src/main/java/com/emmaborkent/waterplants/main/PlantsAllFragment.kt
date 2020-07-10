28069bce425ee36a2403fe4d71fb67bc5681ac18
Emma Borkent <emma_borkent@msn.com>
2020-07-10 14:53:09 +0200

    Added Data Binding to fragment_plants_all.xml and PlantsAllFragment.kt

--- app/src/main/java/com/emmaborkent/waterplants/main/PlantsAllFragment.kt
+++ app/src/main/java/com/emmaborkent/waterplants/main/PlantsAllFragment.kt
@@ -5,48 +5,45 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import com.emmaborkent.waterplants.R
+import com.emmaborkent.waterplants.databinding.FragmentPlantsAllBinding
 import com.emmaborkent.waterplants.plantdetails.PlantDetailsActivity
-import kotlinx.android.synthetic.main.fragment_plants_all.*
 
 class PlantsAllFragment : Fragment() {
-    // Use the 'by activityViewModels()' Kotlin property delegate
-    // from the fragment-ktx artifact
+
+    private lateinit var binding: FragmentPlantsAllBinding
     private val plantViewModel: PlantViewModel by activityViewModels()
-//    private lateinit var plantViewModel: PlantViewModel
+    lateinit var adapter: PlantsAllAdapter
+    lateinit var layoutManager: GridLayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_plants_all, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_plants_all, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-//        plantViewModel = ViewModelProvider(this).get(PlantViewModel::class.java)
+        adapter = PlantsAllAdapter { plant ->
+            plantViewModel.select(plant)
+            goToPlantDetails()
+        }
 
-        val adapter =
-            PlantsAllAdapter { plant ->
-                plantViewModel.select(plant)
-                goToPlantDetails()
-            }
-        recycler_view_all_plants.adapter = adapter
-        recycler_view_all_plants.layoutManager = GridLayoutManager(context, 2)
-        recycler_view_all_plants.setHasFixedSize(true)
+        layoutManager = GridLayoutManager(context, 2)
 
-        // TODO: 25-6-2020 Remove commented out code
-//        plantViewModel = ViewModelProvider(
-//            requireActivity(),
-//            defaultViewModelProviderFactory
-//        ).get(PlantsViewModel::class.java)
+        binding.apply {
+            recyclerViewAllPlants.adapter = adapter
+            recyclerViewAllPlants.layoutManager = layoutManager
+        }
 
-//        plantViewModel = ViewModelProvider(requireActivity()).get(PlantsViewModel::class.java)
         plantViewModel.allPlants.observe(viewLifecycleOwner, Observer { plants ->
             plants?.let { adapter.setPlants(it) }
         })
